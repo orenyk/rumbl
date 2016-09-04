7ecdc85bccfa31ea0de31dfc94782381c191ddd9
Oren Kanner <okanner@gmail.com>
2016-09-04 17:19:08 -0400

    Add User struct with dummy Repo

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,3 +1,26 @@
 defmodule Rumbl.Repo do
-  use Ecto.Repo, otp_app: :rumbl
+  # use Ecto.Repo, otp_app: :rumbl
+  @moduledoc """
+  In-memory repository.
+  """
+
+  def all(Rumbl.User) do
+    [
+      %Rumbl.User{id: "1", name: "John Smith", username: "jsmith1", password: "smithy"},
+      %Rumbl.User{id: "2", name: "Jane Smith", username: "jsmith2", password: "janejane"},
+      %Rumbl.User{id: "3", name: "Foo Bar", username: "foomaster", password: "foofooyou"},
+      %Rumbl.User{id: "4", name: "Oren Kanner", username: "orenyk", password: "you'llneverguess"}
+    ]
+  end
+  def all(_module), do: []
+
+  def get(module, id) do
+    Enum.find(all(module), fn map -> map.id == id end)
+  end
+
+  def get_by(module, params) do
+    Enum.find(all(module), fn map ->
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    end)
+  end
 end
